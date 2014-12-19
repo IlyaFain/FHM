a80d3cff10a753dcc68f59d65c0373c46dd7ff0f
Dmitry Demidovsky <d.demidovsky@gmail.com>
2014-12-19 16:14:29 +0300

    Вёрстка шаблона

---
+++ 00_shablon.php
@@ -0,0 +1,173 @@
+<?
+	define('PAGE_TITLE', 'Шаблон');
+?>
+
+<? require_once('templates/header.php'); ?>
+
+
+<div class="page-gasholders">
+
+
+
+
+	<div class="b-promo" style="background-image: url(img/content/promo_gasholders.jpg)">
+		<div class="b-promo__top-line"></div>
+		<h1 class="b-promo__title">Установки газификации</h1>
+		<div class="b-promo__description">
+			Установки газификации СУГ предназначены для приёма, испарения (газификации) СУГ, нагрева и выдачи газа на газоиспользующее оборудование.
+			<br><br>
+			Установки газификации СУГ предназначены для газификации сжиженных углеводородных газов (СУГ) и снабжения газообразным пропан-бутаном газоиспользующего оборудования в том числе объектов требующих временного газоснабжения на период строительства.
+		</div>
+		<div class="b-promo__bottom-line"></div>
+	</div>
+
+
+
+
+
+	<section class="b-section _no-padding-bottom">
+
+		<div class="b-section__content">
+
+			<div class="b-g__spec _invert">
+
+				<div class="h3">Технические характеристики резервуаров</div>
+
+				<div class="b-tabs">
+					<ul class="b-tabs__contents">
+						<li class="_current">
+							<dl>
+								<dt>Размер</dt><dd>Ну здоровый такой.</dd>
+								<dt>Диапазон рабочих температур</dt><dd>В дубак и в жарищу норм.</dd>
+								<dt>Минимальная доля заполнения</dt><dd>Ну так чтоб чуть-чуть хотя бы.</dd>
+								<dt>Гарантия</dt><dd>Да всё нормально с ним, не парься.</dd>
+							</dl>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+
+		</div>
+
+	</section>
+
+
+
+
+
+
+
+
+	<section class="b-section">
+
+		<h2 class="b-section__title _bottom-bar">Примеры установки</h2>
+
+		<div class="b-section__content">
+
+			<div class="b-examples">
+				<ul class="b-examples__list">
+					<li class="b-examples__item">
+						<a data-litebox data-litebox-group="examples" data-litebox-text="Объект автономного газоснабжения" class="b-examples__link" href="img/examples/1.jpg">
+							<img src="img/examples/1.jpg" alt="">
+							<span class="b-examples__item-text">
+								<b class="b-examples__item-title">Объект автономного газоснабжения частного дома</b>
+								<i class="b-examples__item-subtitle">Поставка оборудования, монтаж, пуско-наладочные работы</i>
+							</span>
+						</a>
+					</li>
+					<li class="b-examples__item">
+						<a data-litebox data-litebox-group="examples" data-litebox-text="Объект автономного газоснабжения" class="b-examples__link" href="img/examples/2.jpg">
+							<img src="img/examples/2.jpg" alt="">
+							<span class="b-examples__item-text">
+								<b class="b-examples__item-title">Объект автономного газоснабжения частного дома автономного газоснабжения частного дома</b>
+								<i class="b-examples__item-subtitle">Поставка оборудования, монтаж, пуско-наладочные работы</i>
+							</span>
+						</a>
+					</li>
+					<li class="b-examples__item">
+						<a data-litebox data-litebox-group="examples" data-litebox-text="Объект автономного газоснабжения" class="b-examples__link" href="img/examples/3.jpg">
+							<img src="img/examples/3.jpg" alt="">
+							<span class="b-examples__item-text">
+								<b class="b-examples__item-title">Объект автономного газоснабжения частного дома</b>
+								<i class="b-examples__item-subtitle">Поставка оборудования, монтаж, пуско-наладочные работы</i>
+							</span>
+						</a>
+					</li>
+					<li class="b-examples__item">
+						<a data-litebox data-litebox-group="examples" data-litebox-text="Объект автономного газоснабжения" class="b-examples__link" href="img/examples/4.jpg">
+							<img src="img/examples/4.jpg" alt="">
+							<span class="b-examples__item-text">
+								<b class="b-examples__item-title">Объект автономного газоснабжения частного дома</b>
+								<i class="b-examples__item-subtitle">Поставка оборудования, монтаж, пуско-наладочные работы</i>
+							</span>
+						</a>
+					</li>
+					<li class="b-examples__item">
+						<a data-litebox data-litebox-group="examples" data-litebox-text="Объект автономного газоснабжения" class="b-examples__link" href="img/examples/5.jpg">
+							<img src="img/examples/5.jpg" alt="">
+							<span class="b-examples__item-text">
+								<b class="b-examples__item-title">Объект автономного газоснабжения частного дома</b>
+								<i class="b-examples__item-subtitle">Поставка оборудования, монтаж, пуско-наладочные работы</i>
+							</span>
+						</a>
+					</li>
+					<li class="b-examples__item">
+						<a data-litebox data-litebox-group="examples" data-litebox-text="Объект автономного газоснабжения" class="b-examples__link" href="img/examples/6.jpg">
+							<img src="img/examples/6.jpg" alt="">
+							<span class="b-examples__item-text">
+								<b class="b-examples__item-title">Объект автономного газоснабжения частного дома</b>
+								<i class="b-examples__item-subtitle">Поставка оборудования, монтаж, пуско-наладочные работы</i>
+							</span>
+						</a>
+					</li>
+					<li class="b-examples__item">
+						<a data-litebox data-litebox-group="examples" data-litebox-text="Объект автономного газоснабжения" class="b-examples__link" href="img/examples/7.jpg">
+							<img src="img/examples/7.jpg" alt="">
+							<span class="b-examples__item-text">
+								<b class="b-examples__item-title">Объект автономного газоснабжения частного дома</b>
+								<i class="b-examples__item-subtitle">Поставка оборудования, монтаж, пуско-наладочные работы</i>
+							</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+
+		</div>
+	</section>
+
+
+
+
+
+
+
+
+	<section class="b-section _no-padding-top">
+
+		<h2 class="b-section__title _bottom-bar">Сертификаты</h2>
+
+		<div class="b-section__content">
+
+			<div class="b-cert">
+				<a data-litebox href="img/certificates/big-1.jpg"><img src="img/certificates/small-1.jpg" alt=""></a>
+				<a data-litebox href="img/certificates/big-2.jpg"><img src="img/certificates/small-2.jpg" alt=""></a>
+				<a data-litebox href="img/certificates/big-3.jpg"><img src="img/certificates/small-3.jpg" alt=""></a>
+			</div>
+
+		</div>
+	</section>
+
+
+
+
+
+
+
+</div>
+
+
+<? antiCache('js/pages/gasholders.js'); ?>
+
+
+<? require_once('templates/footer.php'); ?>
